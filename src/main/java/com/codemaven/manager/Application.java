c37a4a60a50c100578b4e7caf3d11ad79e21867c
matthew_muirhead <matthew.muirhead@live.co.uk>
2020-05-15 18:26:43 +0100

    Setup project

---
+++ src/main/java/com/codemaven/manager/Application.java
@@ -0,0 +1,24 @@
+package com.codemaven.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
+public class Application extends SpringBootServletInitializer
+{
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+	{
+		return application.sources(Application.class);
+	}
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(Application.class, args);
+	}
+}
