56200ea041973d596a86c8bf7dfaf0f277bf54e9
matthew_muirhead <matthew.muirhead@live.co.uk>
2020-05-18 00:50:15 +0100

    Fetch cars in order and save

--- src/main/java/com/codemaven/manager/db/dao/CarDao.java
+++ src/main/java/com/codemaven/manager/db/dao/CarDao.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Repository;
 
 import com.codemaven.generated.Tables;
 import com.codemaven.generated.tables.pojos.Cars;
+import com.codemaven.generated.tables.records.CarsRecord;
+import com.codemaven.manager.model.CarsExtended;
 
 import lombok.AllArgsConstructor;
 
@@ -16,8 +18,19 @@ public class CarDao
 {
 	private final DSLContext dsl;
 	
-	public List<Cars> fetchAllCars()
+	public List<CarsExtended> fetchAllCarsSorted()
 	{
-		return dsl.selectFrom(Tables.CARS).fetchInto(Cars.class);
+		return dsl.selectFrom(Tables.CARS).orderBy(Tables.CARS.MANUFACTURER, Tables.CARS.YEAR).fetchInto(CarsExtended.class);
+	}
+	
+	public CarsExtended fetchCarById(final int id)
+	{
+		return dsl.selectFrom(Tables.CARS).where(Tables.CARS.ID.eq(id)).fetchOneInto(CarsExtended.class);
+	}
+	
+	public boolean saveCar(final Cars car)
+	{
+		CarsRecord record = dsl.newRecord(Tables.CARS, car);
+		return dsl.insertInto(Tables.CARS).set(record).onDuplicateKeyUpdate().set(record).execute() > 0;
 	}
 }
--- src/main/java/com/codemaven/manager/db/service/CarService.java
+++ src/main/java/com/codemaven/manager/db/service/CarService.java
@@ -8,6 +8,7 @@ import com.codemaven.generated.tables.pojos.Cars;
 import com.codemaven.manager.db.Service;
 import com.codemaven.manager.db.ServiceType;
 import com.codemaven.manager.db.dao.CarDao;
+import com.codemaven.manager.model.CarsExtended;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,9 +20,29 @@ public class CarService implements Service
 {
 	private CarDao dao;
 	
-	public List<Cars> fetchAllCars()
+	public List<CarsExtended> fetchAllCarsSorted()
 	{
-		return dao.fetchAllCars();
+		return dao.fetchAllCarsSorted();
+	}
+	
+	public CarsExtended fetchCarById(final int id)
+	{
+		CarsExtended car = null;
+		if (id > 0)
+		{
+			car = dao.fetchCarById(id);
+		}
+		return car;
+	}
+	
+	public boolean saveCar(final Cars car)
+	{
+		boolean saved = false;
+		if (car != null)
+		{
+			saved = dao.saveCar(car);
+		}
+		return saved;
 	}
 	
 	@Override
