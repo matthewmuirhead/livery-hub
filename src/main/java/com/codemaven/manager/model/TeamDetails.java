1e296e31ad2cc964d35a74452e77d93c7a6fd603
matthew_muirhead <matthew.muirhead@live.co.uk>
2020-05-19 00:40:32 +0100

    Create wrapper class containing all team information

---
+++ src/main/java/com/codemaven/manager/model/TeamDetails.java
@@ -0,0 +1,72 @@
+package com.codemaven.manager.model;
+
+import java.util.List;
+
+import com.codemaven.generated.tables.pojos.TeamFuel;
+import com.codemaven.generated.tables.pojos.TeamTires;
+import com.codemaven.generated.tables.pojos.Teams;
+import com.codemaven.manager.db.service.TeamService;
+
+import lombok.Setter;
+
+@Setter
+public class TeamDetails
+{
+	private TeamService teamService;
+	private int teamId;
+	private Teams team;
+	private List<TeamFuel> fuel;
+	private List<TeamTires> tires;
+	
+	public TeamDetails(TeamService teamService, int teamId)
+	{
+		this.teamService = teamService;
+		this.teamId = teamId;
+	}
+	
+	public TeamDetails(TeamService teamService, Teams team)
+	{
+		this.teamService = teamService;
+		this.team = team;
+		this.teamId = team.getId();
+	}
+	
+	public void populate()
+	{
+		getTeam();
+		getFuel();
+		getTires();
+	}
+	
+	public int getTeamId()
+	{
+		return teamId;
+	}
+	
+	public Teams getTeam()
+	{
+		if (team == null)
+		{
+			team = teamService.fetchTeamById(teamId);
+		}
+		return team;
+	}
+	
+	public List<TeamFuel> getFuel()
+	{
+		if (fuel == null)
+		{
+			fuel = teamService.fetchTeamFuelByTeamId(teamId);
+		}
+		return fuel;
+	}
+	
+	public List<TeamTires> getTires()
+	{
+		if (tires == null)
+		{
+			tires = teamService.fetchTeamTiresByTeamId(teamId);
+		}
+		return tires;
+	}
+}
