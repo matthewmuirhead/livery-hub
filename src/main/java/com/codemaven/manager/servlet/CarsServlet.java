ddeef1d8dfcb26a146d6480f57ddf2888a296b18
matthew_muirhead <matthew.muirhead@live.co.uk>
2020-05-18 01:02:57 +0100

    Create cars servlet with full functionality

---
+++ src/main/java/com/codemaven/manager/servlet/CarsServlet.java
@@ -0,0 +1,156 @@
+package com.codemaven.manager.servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.codemaven.generated.tables.pojos.Cars;
+import com.codemaven.manager.db.ServiceFactory;
+import com.codemaven.manager.db.ServiceType;
+import com.codemaven.manager.db.service.CarService;
+import com.codemaven.manager.enums.NavBarZone;
+import com.codemaven.manager.model.AjaxSaveReplyJson;
+import com.codemaven.manager.model.CarouselDisplayItem;
+import com.codemaven.manager.model.CarsExtended;
+import com.codemaven.manager.util.StringUtil;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping(value = "/cars")
+@Slf4j
+public class CarsServlet extends ServletBase
+{
+	private ServiceFactory serviceFactory;
+	
+	@Override
+	public void processRequest(HttpServletRequest req, HttpServletResponse resp) throws IOException
+	{
+		String cmd = getCmd(req);
+		if (StringUtil.isNullOrEmpty(cmd) || StringUtil.isEqual(cmd, "list"))
+		{
+			doList(req, resp);
+		}
+		else if (StringUtil.isEqual(cmd, "view"))
+		{
+			doView(req, resp);
+		}
+		else if (StringUtil.isEqual(cmd, "new"))
+		{
+			doNew(req, resp);
+		}
+		else if (StringUtil.isEqual(cmd, "save"))
+		{
+			doSave(req, resp, false);
+		}
+		else if (StringUtil.isEqual(cmd, "ajaxSave"))
+		{
+			doSave(req, resp, true);
+		}
+	}
+	
+	private void doList(HttpServletRequest req, HttpServletResponse resp)
+	{
+		List<CarsExtended> cars = serviceFactory.getInstance(ServiceType.CAR, CarService.class).fetchAllCarsSorted();
+		log.debug(cars.size() + " cars loaded");
+		req.setAttribute("cars", cars);
+		
+		List<CarouselDisplayItem> carousel = new ArrayList<>();
+		CarouselDisplayItem displayItem = new CarouselDisplayItem();
+		displayItem.setAltText("Audi R8 Tail");
+		displayItem.setUrl("audi-tail.png");
+		carousel.add(displayItem);
+		CarouselDisplayItem displayItem2 = new CarouselDisplayItem();
+		displayItem2.setAltText("Nissan GTR Tail");
+		displayItem2.setUrl("gtr-tail-pits.png");
+		carousel.add(displayItem2);
+		req.setAttribute("carousel", carousel);
+		
+		req.setAttribute("title", "Cars");
+		displayPage(req, resp, "cars/list.jsp");
+	}
+	
+	private void doView(HttpServletRequest req, HttpServletResponse resp)
+	{
+		doView(req, resp, getParameterInt(req, "id"));
+	}
+	
+	private void doView(HttpServletRequest req, HttpServletResponse resp, int carId)
+	{
+		CarsExtended car = serviceFactory.getInstance(ServiceType.CAR, CarService.class).fetchCarById(carId);
+		if (car != null)
+		{
+			req.setAttribute("car", car);
+			req.setAttribute("title", car.getFullNameAndYear());
+			displayPage(req, resp, "cars/view.jsp");
+		}
+		else
+		{
+			doList(req, resp);
+		}
+	}
+	
+	private void doNew(HttpServletRequest req, HttpServletResponse resp)
+	{
+		Cars car = new Cars();
+		req.setAttribute("car", car);
+		req.setAttribute("title", "New Car");
+		displayPage(req, resp, "cars/view.jsp");
+	}
+	
+	private void doSave(HttpServletRequest req, HttpServletResponse resp, boolean isAjax) throws IOException
+	{
+ 		AjaxSaveReplyJson replyJson = new AjaxSaveReplyJson();
+ 		CarsExtended car = new CarsExtended();
+		car.setId(getParameterInt(req, "carId"));
+		car.setManufacturer(getParameterString(req, "manufacturer"));
+		car.setModel(getParameterString(req, "model"));
+		car.setYear(getParameterInt(req, "year"));
+		car.setLogo(getParameterString(req, "logo"));
+		car.setImage(getParameterString(req, "image"));
+		boolean saved = serviceFactory.getInstance(ServiceType.CAR, CarService.class).saveCar(car);
+		if (isAjax)
+		{
+			if (saved)
+			{
+				// Success Ajax Response
+				replyJson.addErrorMessage(AjaxSaveReplyJson.SUCCESS_KEY, "Car " + car.getFullName() + " saved successfully.");
+				resp.setContentType("application/json");
+				resp.getWriter().write(replyJson.toJsonString());
+			}
+			else
+			{
+				// Failed Ajax Response
+				replyJson.addErrorMessage(AjaxSaveReplyJson.GENERAL_ERROR, "Could not save car " + car.getFullName() + ".");
+				resp.setContentType("application/json");
+				resp.getWriter().write(replyJson.toJsonString());
+			}
+		}
+		else
+		{
+			if (saved)
+			{
+				doList(req, resp);
+			}
+			else
+			{
+				req.setAttribute(AjaxSaveReplyJson.GENERAL_ERROR, "Could not save car " + car.getFullName() + ".");
+				doView(req, resp, car.getId());
+			}
+		}
+	}
+
+	@Override
+	protected NavBarZone getNavBarZone()
+	{
+		return NavBarZone.CARS;
+	}
+}
