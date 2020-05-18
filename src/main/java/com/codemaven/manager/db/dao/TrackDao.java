688bc20720ece62874a856849ab290878ae5c157
matthew_muirhead <matthew.muirhead@live.co.uk>
2020-05-18 01:04:03 +0100

    Create functionality to show available tracks

---
+++ src/main/java/com/codemaven/manager/db/dao/TrackDao.java
@@ -0,0 +1,40 @@
+package com.codemaven.manager.db.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.jooq.DSLContext;
+import org.springframework.stereotype.Repository;
+
+import com.codemaven.generated.Tables;
+import com.codemaven.generated.tables.pojos.Locations;
+import com.codemaven.generated.tables.pojos.Tracks;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Repository
+public class TrackDao
+{
+	private final DSLContext dsl;
+
+	public List<Tracks> fetchAllTracks()
+	{
+		return dsl.selectFrom(Tables.TRACKS)
+				.fetchInto(Tracks.class);
+	}
+
+	public List<Tracks> fetchTracksBySet(String set)
+	{
+		return dsl.selectFrom(Tables.TRACKS)
+				.where(Tables.TRACKS.SET.eq(set))
+				.fetchInto(Tracks.class);
+	}
+	
+	public Map<Tracks, Locations> fetchTracksLocationsBySet(String set)
+	{
+		return dsl.selectFrom(Tables.TRACKS.join(Tables.LOCATIONS).on(Tables.TRACKS.LOCATION_ID.eq(Tables.LOCATIONS.ID)))
+				.where(Tables.TRACKS.SET.eq(set))
+				.fetchMap(Tracks.class, Locations.class);
+	}
+}
---
+++ src/main/java/com/codemaven/manager/db/service/TrackService.java
@@ -0,0 +1,65 @@
+package com.codemaven.manager.db.service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+import org.testcontainers.shaded.io.netty.util.internal.StringUtil;
+
+import com.codemaven.generated.tables.pojos.Locations;
+import com.codemaven.generated.tables.pojos.Tracks;
+import com.codemaven.manager.db.Service;
+import com.codemaven.manager.db.ServiceType;
+import com.codemaven.manager.db.dao.TrackDao;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@AllArgsConstructor
+@Component
+@Slf4j
+public class TrackService implements Service
+{
+	private TrackDao dao;
+	
+	public List<Tracks> fetchAllTracks()
+	{
+		return dao.fetchAllTracks();
+	}
+	
+	public List<Tracks> fetchTracksBySet(String set)
+	{
+		List<Tracks> tracks = new ArrayList<>();
+		if (!StringUtil.isNullOrEmpty(set))
+		{
+			tracks = dao.fetchTracksBySet(set);
+		}
+		else if (log.isDebugEnabled())
+		{
+			log.debug("Tried fetching tracks with set " + set);
+		}
+		return tracks;
+	}
+	
+	public Map<Tracks, Locations> fetchTracksLocationsBySet(String set)
+	{
+		Map<Tracks, Locations> tracks = new HashMap<>();
+		if (!StringUtil.isNullOrEmpty(set))
+		{
+			tracks = dao.fetchTracksLocationsBySet(set);
+		}
+		else if (log.isDebugEnabled())
+		{
+			log.debug("Tried fetching tracks with set " + set);
+		}
+		return tracks;
+	}
+	
+	@Override
+	public ServiceType getType()
+	{
+		return ServiceType.TRACK;
+	}
+}
