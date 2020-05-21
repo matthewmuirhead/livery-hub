397512dd9066c02f701fb17d3a78422292165e47
matthew_muirhead <matthew.muirhead@live.co.uk>
2020-05-21 21:16:33 +0100

    Add edit and save functionality

--- src/main/java/com/codemaven/manager/servlet/EventsServlet.java
+++ src/main/java/com/codemaven/manager/servlet/EventsServlet.java
@@ -13,11 +13,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.codemaven.generated.tables.pojos.Events;
+import com.codemaven.generated.tables.pojos.Hosts;
+import com.codemaven.generated.tables.pojos.Tracks;
 import com.codemaven.manager.db.ServiceFactory;
 import com.codemaven.manager.db.ServiceType;
-import com.codemaven.manager.db.service.EventService;
+import com.codemaven.manager.db.service.EventsService;
+import com.codemaven.manager.db.service.HostsService;
+import com.codemaven.manager.db.service.TracksService;
 import com.codemaven.manager.enums.NavBarZone;
 import com.codemaven.manager.lists.EventDetailsList;
+import com.codemaven.manager.model.AjaxSaveReplyJson;
 import com.codemaven.manager.model.CarouselDisplayItem;
 import com.codemaven.manager.model.EventDetails;
 import com.codemaven.manager.util.StringUtil;
@@ -35,7 +40,19 @@ public class EventsServlet extends ServletBase
 	private ServiceFactory serviceFactory;
 	
 	@Override
-	public void processRequest(HttpServletRequest req, HttpServletResponse resp) throws IOException
+	public void processRequest(HttpServletRequest req, HttpServletResponse resp)
+	{
+		try {
+			doCmd(req, resp);
+		}
+		catch (Exception e)
+		{
+			log.error("Error processing cmd: " + e.getMessage(), e);
+			displayError(req, resp, "Uh-Oh, something went wrong!");
+		}
+	}
+	
+	private void doCmd(HttpServletRequest req, HttpServletResponse resp) throws IOException
 	{
 		String cmd = getCmd(req);
 		if (StringUtil.isNullOrEmpty(cmd) || StringUtil.isEqual(cmd, "calendar"))
@@ -87,7 +104,7 @@ public class EventsServlet extends ServletBase
 		
 		LocalDateTime from = LocalDateTime.now().with(TemporalAdjusters.firstDayOfMonth()).withHour(0).withMinute(0);
 		LocalDateTime to = LocalDateTime.now().with(TemporalAdjusters.lastDayOfMonth()).withHour(23).withMinute(59);
-		List<Events> events = serviceFactory.getInstance(ServiceType.EVENT, EventService.class).fetchEventsBetweenDates(from, to);
+		List<Events> events = serviceFactory.getInstance(ServiceType.EVENT, EventsService.class).fetchEventsBetweenDates(from, to);
 		EventDetailsList eventDetailsList = new EventDetailsList(events, serviceFactory);
 		
 		req.setAttribute("eventDetails", eventDetailsList);
@@ -114,13 +131,15 @@ public class EventsServlet extends ServletBase
 		carousel.add(displayItem2);
 		req.setAttribute("carousel", carousel);
 		
-		LocalDateTime from = LocalDateTime.now();
-		LocalDateTime to = LocalDateTime.now().with(TemporalAdjusters.lastDayOfMonth()).withHour(23).withMinute(59);
-		List<Events> events = serviceFactory.getInstance(ServiceType.EVENT, EventService.class).fetchEventsBetweenDates(from, to);
+		LocalDateTime from = LocalDateTime.now()
+				.withHour(0)
+				.withMinute(0)
+				.withSecond(0)
+				.withNano(0);
+		List<Events> events = serviceFactory.getInstance(ServiceType.EVENT, EventsService.class).fetchEventsAfterDate(from);
 		EventDetailsList eventDetailsList = new EventDetailsList(events, serviceFactory);
-		
 		req.setAttribute("eventDetails", eventDetailsList);
-		req.setAttribute("currentMonth", from.getMonth());
+		
 		req.setAttribute("title", "Events List");
 		displayPage(req, resp, JSP_PATH+"list.jsp");
 	}
@@ -128,26 +147,96 @@ public class EventsServlet extends ServletBase
 	private void doView(HttpServletRequest req, HttpServletResponse resp)
 	{
 		int eventId = getParameterInt(req, "id");
+		doView(req, resp, eventId);
+	}
+	
+	private void doView(HttpServletRequest req, HttpServletResponse resp, int id)
+	{
+		int eventId = getParameterInt(req, "id");
 		EventDetails eventDetails = new EventDetails(serviceFactory, eventId);
 		eventDetails.populate();
 		req.setAttribute("eventDetails", eventDetails);
-		req.setAttribute("title", "Events List");
+		req.setAttribute("title", "View "+eventDetails.getEvent().getName());
 		displayPage(req, resp, JSP_PATH+"view.jsp");
 	}
 	
 	private void doNew(HttpServletRequest req, HttpServletResponse resp)
 	{
+		req.setAttribute("title", "New Event");
+		displayPage(req, resp, JSP_PATH+"edit.jsp");
 		
+		List<Tracks> tracks = serviceFactory.getInstance(ServiceType.TRACK, TracksService.class).fetchAllTracks();
+		req.setAttribute("tracks", tracks);
+		
+		List<Hosts> hosts = serviceFactory.getInstance(ServiceType.HOST, HostsService.class).fetchAllHosts();
+		req.setAttribute("hosts", hosts);
 	}
 	
 	private void doEdit(HttpServletRequest req, HttpServletResponse resp)
 	{
+		int eventId = getParameterInt(req, "id");
+		EventDetails eventDetails = new EventDetails(serviceFactory, eventId);
+		eventDetails.populate();
+		req.setAttribute("eventDetails", eventDetails);
 		
+		List<Tracks> tracks = serviceFactory.getInstance(ServiceType.TRACK, TracksService.class).fetchAllTracks();
+		req.setAttribute("tracks", tracks);
+		
+		List<Hosts> hosts = serviceFactory.getInstance(ServiceType.HOST, HostsService.class).fetchAllHosts();
+		req.setAttribute("hosts", hosts);
+		
+		req.setAttribute("title", "Edit "+eventDetails.getEvent().getName());
+		displayPage(req, resp, JSP_PATH+"edit.jsp");
 	}
 	
-	private void doSave(HttpServletRequest req, HttpServletResponse resp, boolean isAjax)
+	private void doSave(HttpServletRequest req, HttpServletResponse resp, boolean isAjax) throws IOException
 	{
-		
+		AjaxSaveReplyJson replyJson = new AjaxSaveReplyJson();
+		Events event = populateEventFromRequest(req);
+		EventDetails eventDetails = new EventDetails(serviceFactory, event);
+		boolean saved = eventDetails.save();
+		if (isAjax)
+		{
+			if (saved)
+			{
+				// Success Ajax Response
+				replyJson.addErrorMessage(AjaxSaveReplyJson.SUCCESS_KEY, "Event " + event.getName() + " saved successfully.");
+				resp.setContentType("application/json");
+				resp.getWriter().write(replyJson.toJsonString());
+			}
+			else
+			{
+				// Failed Ajax Response
+				replyJson.addErrorMessage(AjaxSaveReplyJson.GENERAL_ERROR, "Could not save event " + event.getName() + ".");
+				resp.setContentType("application/json");
+				resp.getWriter().write(replyJson.toJsonString());
+			}
+		}
+		else
+		{
+			if (saved)
+			{
+				doList(req, resp);
+			}
+			else
+			{
+				req.setAttribute(AjaxSaveReplyJson.GENERAL_ERROR, "Could not save event " + event.getName() + ".");
+				doView(req, resp, event.getId());
+			}
+		}
+	}
+	
+	private Events populateEventFromRequest(HttpServletRequest req)
+	{
+		Events event = new Events();
+		event.setId(getParameterInt(req, "id"));
+		event.setName(getParameterString(req, "name"));
+		event.setDescription(getParameterString(req, "description"));
+		event.setRegulations(getParameterString(req, ""));
+		event.setHostId(getParameterInt(req, "hostId"));
+		event.setTrackId(getParameterInt(req, "trackId"));
+//		event.setEventDate();
+		return event;
 	}
 	
 	@Override
