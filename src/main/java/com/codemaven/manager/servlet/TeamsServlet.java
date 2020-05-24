72d5eb3e1c52bf65be22521496192461a0a6c80e
matthew_muirhead <matthew.muirhead@live.co.uk>
2020-05-24 20:40:43 +0100

    List and edit teams

--- src/main/java/com/codemaven/manager/servlet/TeamsServlet.java
+++ src/main/java/com/codemaven/manager/servlet/TeamsServlet.java
@@ -1,6 +1,8 @@
 package com.codemaven.manager.servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -8,10 +10,16 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.codemaven.generated.tables.pojos.Drivers;
+import com.codemaven.generated.tables.pojos.Teams;
 import com.codemaven.manager.db.ServiceFactory;
 import com.codemaven.manager.db.ServiceType;
+import com.codemaven.manager.db.service.CarsService;
+import com.codemaven.manager.db.service.DriversService;
 import com.codemaven.manager.db.service.TeamsService;
 import com.codemaven.manager.enums.NavBarZone;
+import com.codemaven.manager.model.CarsExtended;
+import com.codemaven.manager.model.TeamDetails;
 import com.codemaven.manager.util.StringUtil;
 
 import lombok.AllArgsConstructor;
@@ -45,12 +53,62 @@ public class TeamsServlet extends ServletBase
 		if (StringUtil.isNullOrEmpty(cmd) || StringUtil.isEqual(cmd, "list")) {
 			doList(req, resp);
 		}
+		else if (StringUtil.isEqual(cmd, "new")) {
+			doNew(req, resp);
+		}
+		else if (StringUtil.isEqual(cmd, "edit")) {
+			doEdit(req, resp);
+		}
+		else if (StringUtil.isEqual(cmd, "save")) {
+			doSave(req, resp, false);
+		}
+		else if (StringUtil.isEqual(cmd, "ajaxSave")) {
+			doSave(req, resp, true);
+		}
 		else if (StringUtil.isEqual(cmd, "ajaxDelete")) {
 			doDelete(req, resp, true);
 		}
 	}
 	
 	private void doList(HttpServletRequest req, HttpServletResponse resp)
+	{
+		List<Teams> teams = serviceFactory.getInstance(ServiceType.TEAM, TeamsService.class).fetchAllTeams();
+		List<TeamDetails> teamDetailsList = new ArrayList<>();
+		for (Teams team : teams)
+		{
+			TeamDetails teamDetails = new TeamDetails(serviceFactory, team);
+			teamDetailsList.add(teamDetails);
+		}
+		req.setAttribute("teamDetailsList", teamDetailsList);
+		req.setAttribute("title", "Teams List");
+		displayPage(req, resp, JSP_PATH+"/list.jsp");
+	}
+	
+	private void doNew(HttpServletRequest req, HttpServletResponse resp)
+	{
+		List<CarsExtended> cars = serviceFactory.getInstance(ServiceType.CAR, CarsService.class).fetchAllCarsSorted();
+		req.setAttribute("cars", cars);
+		List<Drivers> drivers = serviceFactory.getInstance(ServiceType.DRIVER, DriversService.class).fetchAllDrivers();
+		req.setAttribute("drivers", drivers);
+		req.setAttribute("title", "New Team");
+		displayPage(req, resp, JSP_PATH+"/edit.jsp");
+	}
+	
+	private void doEdit(HttpServletRequest req, HttpServletResponse resp)
+	{
+		int teamId = getParameterInt(req, "id");
+		Teams team = serviceFactory.getInstance(ServiceType.TEAM, TeamsService.class).fetchTeamById(teamId);
+		TeamDetails teamDetails = new TeamDetails(serviceFactory, team);
+		req.setAttribute("teamDetails", teamDetails);
+		List<CarsExtended> cars = serviceFactory.getInstance(ServiceType.CAR, CarsService.class).fetchAllCarsSorted();
+		req.setAttribute("cars", cars);
+		List<Drivers> drivers = serviceFactory.getInstance(ServiceType.DRIVER, DriversService.class).fetchAllDrivers();
+		req.setAttribute("drivers", drivers);
+		req.setAttribute("title", "Edit Team: "+team.getName());
+		displayPage(req, resp, JSP_PATH+"/edit.jsp");
+	}
+	
+	private void doSave(HttpServletRequest req, HttpServletResponse resp, boolean isAjax)
 	{
 		
 	}
