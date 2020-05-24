2b0dfa081f9e8fd2f0ccd9188a9097954a15292b
matthew_muirhead <matthew.muirhead@live.co.uk>
2020-05-24 01:34:30 +0100

    Add drivers and car to team details

--- src/main/java/com/codemaven/manager/model/TeamDetails.java
+++ src/main/java/com/codemaven/manager/model/TeamDetails.java
@@ -1,10 +1,16 @@
 package com.codemaven.manager.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import com.codemaven.generated.tables.pojos.Drivers;
 import com.codemaven.generated.tables.pojos.TeamFuel;
 import com.codemaven.generated.tables.pojos.TeamTires;
 import com.codemaven.generated.tables.pojos.Teams;
+import com.codemaven.manager.db.ServiceFactory;
+import com.codemaven.manager.db.ServiceType;
+import com.codemaven.manager.db.service.CarsService;
+import com.codemaven.manager.db.service.DriversService;
 import com.codemaven.manager.db.service.TeamsService;
 
 import lombok.Setter;
@@ -12,21 +18,29 @@ import lombok.Setter;
 @Setter
 public class TeamDetails
 {
-	private TeamsService teamService;
+	private TeamsService teamsService;
+	private DriversService driversService;
+	private CarsService carsService;
 	private int teamId;
 	private Teams team;
+	private List<Drivers> drivers;
 	private List<TeamFuel> fuel;
 	private List<TeamTires> tires;
+	private CarsExtended car;
 	
-	public TeamDetails(TeamsService teamService, int teamId)
+	public TeamDetails(ServiceFactory serviceFactory, int teamId)
 	{
-		this.teamService = teamService;
+		this.teamsService = serviceFactory.getInstance(ServiceType.TEAM, TeamsService.class);
+		this.driversService = serviceFactory.getInstance(ServiceType.DRIVER, DriversService.class);
+		this.carsService = serviceFactory.getInstance(ServiceType.CAR, CarsService.class);
 		this.teamId = teamId;
 	}
 	
-	public TeamDetails(TeamsService teamService, Teams team)
+	public TeamDetails(ServiceFactory serviceFactory, Teams team)
 	{
-		this.teamService = teamService;
+		this.teamsService = serviceFactory.getInstance(ServiceType.TEAM, TeamsService.class);
+		this.driversService = serviceFactory.getInstance(ServiceType.DRIVER, DriversService.class);
+		this.carsService = serviceFactory.getInstance(ServiceType.CAR, CarsService.class);
 		this.team = team;
 		this.teamId = team.getId();
 	}
@@ -34,8 +48,10 @@ public class TeamDetails
 	public void populate()
 	{
 		getTeam();
+		getDrivers();
 		getFuel();
 		getTires();
+		getCar();
 	}
 	
 	public int getTeamId()
@@ -47,7 +63,7 @@ public class TeamDetails
 	{
 		if (team == null)
 		{
-			team = teamService.fetchTeamById(teamId);
+			team = teamsService.fetchTeamById(teamId);
 		}
 		return team;
 	}
@@ -56,7 +72,7 @@ public class TeamDetails
 	{
 		if (fuel == null)
 		{
-			fuel = teamService.fetchTeamFuelByTeamId(teamId);
+			fuel = teamsService.fetchTeamFuelByTeamId(teamId);
 		}
 		return fuel;
 	}
@@ -65,8 +81,52 @@ public class TeamDetails
 	{
 		if (tires == null)
 		{
-			tires = teamService.fetchTeamTiresByTeamId(teamId);
+			tires = teamsService.fetchTeamTiresByTeamId(teamId);
 		}
 		return tires;
+	}
+	
+	public CarsExtended getCar()
+	{
+		if (car == null)
+		{
+			car = carsService.fetchCarById(getTeam().getCarId());
+		}
+		return car;
+	}
+	
+	public List<Drivers> getDrivers()
+	{
+		if (drivers == null || drivers.size() == 0)
+		{
+			drivers = new ArrayList<>();
+			Teams team = getTeam();
+			addDriver(drivers, driversService.fetchDriverById(team.getDriver_1()));
+			addDriver(drivers, driversService.fetchDriverById(team.getDriver_2()));
+			addDriver(drivers, driversService.fetchDriverById(team.getDriver_3()));
+			addDriver(drivers, driversService.fetchDriverById(team.getDriver_4()));
+		}
+		return drivers;
+	}
+	
+	private void addDriver(List<Drivers> drivers, Drivers driver)
+	{
+		if (driver != null) 
+		{
+			drivers.add(driver);
+		}
+	}
+	
+	public String getStatusStyle()
+	{
+		switch (getTeam().getStatus())
+		{
+			case "Confirmed":
+				return "border-success";
+			case "Provisional":
+				return "border-warning";
+			default:
+				return "border-danger";
+		}
 	}
 }
