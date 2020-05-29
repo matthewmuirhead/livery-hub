ca797eb50d59eb2604c053f79fd3a7aa271ac9a2
matthew_muirhead <matthew.muirhead@live.co.uk>
2020-05-29 01:23:38 +0100

    Create empty servlets for corresponding URLs

---
+++ src/main/java/com/codemaven/events/admin/servlet/EventsServlet.java
@@ -0,0 +1,66 @@
+package com.codemaven.events.admin.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.codemaven.events.db.ServiceFactory;
+import com.codemaven.events.manager.enums.NavBarZone;
+import com.codemaven.events.servlet.ServletBase;
+import com.codemaven.events.util.StringUtil;
+import com.codemaven.generated.tables.pojos.Users;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping(value = "/admin/events")
+@Slf4j
+public class EventsServlet extends ServletBase
+{
+	private static final String JSP_PATH = "admin/events";
+	private ServiceFactory serviceFactory;
+	
+	@Override
+	public void processRequest(HttpServletRequest req, HttpServletResponse resp)
+	{
+		try
+		{
+			Users user = getLoggedInUser(req);
+			if (user == null || !user.getAdmin())
+			{
+				// Redirect to homepage
+				displayPage(req, resp, "", true);
+			}
+			else
+			{
+				doCmd(req, resp);
+			}
+		}
+		catch (Exception e)
+		{
+			log.error("Error processing cmd: " + e.getMessage(), e);
+			displayError(req, resp, "Uh-Oh, something went wrong!");
+		}
+	}
+	
+	private void doCmd(HttpServletRequest req, HttpServletResponse resp) throws IOException
+	{
+		String cmd = getCmd(req);
+		if (StringUtil.isNullOrEmpty(cmd))
+		{
+			
+		}
+	}
+	
+	@Override
+	protected NavBarZone getNavBarZone()
+	{
+		return NavBarZone.ADMIN_EVENTS;
+	}
+}
---
+++ src/main/java/com/codemaven/events/servlet/EventsServlet.java
@@ -0,0 +1,55 @@
+package com.codemaven.events.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.codemaven.events.db.ServiceFactory;
+import com.codemaven.events.manager.enums.NavBarZone;
+import com.codemaven.events.util.StringUtil;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping(value = "/events")
+@Slf4j
+public class EventsServlet extends ServletBase
+{
+	private static final String JSP_PATH = "events";
+	private ServiceFactory serviceFactory;
+	
+	@Override
+	public void processRequest(HttpServletRequest req, HttpServletResponse resp)
+	{
+		try
+		{
+			doCmd(req, resp);
+		}
+		catch (Exception e)
+		{
+			log.error("Error processing cmd: " + e.getMessage(), e);
+			displayError(req, resp, "Uh-Oh, something went wrong!");
+		}
+	}
+	
+	private void doCmd(HttpServletRequest req, HttpServletResponse resp) throws IOException
+	{
+		String cmd = getCmd(req);
+		if (StringUtil.isNullOrEmpty(cmd))
+		{
+			
+		}
+	}
+	
+	@Override
+	protected NavBarZone getNavBarZone()
+	{
+		return NavBarZone.EVENTS;
+	}
+}
