5c869f757aa6709ff46ec6b9d769684d59dfef6b
Matt Muirhead <matthew.muirhead@live.co.uk>
2021-06-30 22:49:29 +0100

    set language fields list to request

--- src/main/java/com/codemaven/liveries/filter/LiveryConcierge.java
+++ src/main/java/com/codemaven/liveries/filter/LiveryConcierge.java
@@ -14,6 +14,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import com.codemaven.liveries.manager.enums.NavBarZone;
+import com.codemaven.liveries.manager.lists.LanguageFieldsList;
 import com.codemaven.liveries.servlet.ServletBase;
 import com.codemaven.liveries.util.StringUtil;
 
@@ -45,6 +46,7 @@ public class LiveryConcierge extends ServletBase implements Filter
 			if (checkIfAdminServletIsSupported(servletName))
 			{
 				log.info("Manager Servlet " + servletName + " Supported, continuing...");
+				req.setAttribute("languageFieldsList", new LanguageFieldsList());
 				chain.doFilter(req, resp);
 			}
 			else
@@ -61,6 +63,7 @@ public class LiveryConcierge extends ServletBase implements Filter
 			if (checkIfServletIsSupported(servletName))
 			{
 				log.info("Servlet " + servletName + " Supported, continuing...");
+				req.setAttribute("languageFieldsList", new LanguageFieldsList());
 				chain.doFilter(req, resp);
 			}
 			else if (isImageAccess(url) || StringUtil.isEqual("/favicon.ico", url))
