d976e525d83cab36bd22489d8a07509fbf6a0022
Matt Muirhead <matthew.muirhead@live.co.uk>
2021-06-30 16:43:45 +0100

    Add dashboard servlet as landing page

---
+++ src/main/java/com/codemaven/liveries/servlet/DashboardServlet.java
@@ -0,0 +1,55 @@
+package com.codemaven.liveries.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.codemaven.liveries.db.ServiceFactory;
+import com.codemaven.liveries.manager.enums.NavBarZone;
+import com.codemaven.liveries.util.StringUtil;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping(value = "/")
+@Slf4j
+public class DashboardServlet extends ServletBase
+{
+	private static final String JSP_PATH = "dashboard";
+	private ServiceFactory serviceFactory;
+
+	@Override
+	public void processRequest(HttpServletRequest req, HttpServletResponse resp)
+	{
+		try
+		{
+			doCmd(req, resp);
+		}
+		catch (Exception e)
+		{
+			log.error("Error processing cmd: " + e.getMessage(), e);
+			displayError(req, resp, "Uh-Oh, something went wrong!");
+		}
+	}
+
+	private void doCmd(HttpServletRequest req, HttpServletResponse resp) throws IOException
+	{
+		String cmd = getCmd(req);
+		if (StringUtil.isNullOrEmpty(cmd))
+		{
+
+		}
+	}
+
+	@Override
+	protected NavBarZone getNavBarZone()
+	{
+		return NavBarZone.DASHBOARD;
+	}
+}
