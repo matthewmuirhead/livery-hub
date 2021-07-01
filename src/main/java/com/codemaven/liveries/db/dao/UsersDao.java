854318e3a8b5855224bd598e6dffc5704a8bb2a2
Matt Muirhead <matthew.muirhead@live.co.uk>
2021-07-01 22:14:59 +0100

    Add methods related to logging in user

--- src/main/java/com/codemaven/liveries/db/dao/UsersDao.java
+++ src/main/java/com/codemaven/liveries/db/dao/UsersDao.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Repository;
 
 import com.codemaven.generated.Tables;
 import com.codemaven.generated.tables.pojos.Users;
+import com.codemaven.generated.tables.records.UsersRecord;
+import com.codemaven.liveries.model.ExtendedUser;
 
 import lombok.AllArgsConstructor;
 
@@ -14,10 +16,27 @@ public class UsersDao
 {
 	private final DSLContext dsl;
 	
-	public Users fetchUserByUsername(final String username)
+	public ExtendedUser fetchUserByUsername(final String username)
 	{
 		return dsl.selectFrom(Tables.USERS)
 				.where(Tables.USERS.USERNAME.eq(username))
-				.fetchOneInto(Users.class);
+				.fetchOneInto(ExtendedUser.class);
+	}
+	
+	public boolean userExists(final String username)
+	{
+		return dsl.fetchExists(dsl.select(Tables.USERS.ID)
+				.from(Tables.USERS)
+				.where(Tables.USERS.USERNAME.eq(username)));
+	}
+	
+	public boolean saveUser(final Users user)
+	{
+		UsersRecord record = dsl.newRecord(Tables.USERS, user);
+		return dsl.insertInto(Tables.USERS)
+				.set(record)
+				.onDuplicateKeyUpdate()
+				.set(record)
+				.execute() > 0;
 	}
 }
