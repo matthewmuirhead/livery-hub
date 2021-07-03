461847f91ee38215713cb4f44f814c66fc164d16
Matt Muirhead <matthew.muirhead@live.co.uk>
2021-07-03 20:25:30 +0100

    Load required values and display jsps

--- src/main/java/com/codemaven/liveries/servlet/SeriesServlet.java
+++ src/main/java/com/codemaven/liveries/servlet/SeriesServlet.java
@@ -1,6 +1,7 @@
 package com.codemaven.liveries.servlet;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -8,7 +9,12 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.codemaven.generated.tables.pojos.Series;
+import com.codemaven.generated.tables.pojos.Teams;
 import com.codemaven.liveries.db.ServiceFactory;
+import com.codemaven.liveries.db.ServiceType;
+import com.codemaven.liveries.db.service.SeriesService;
+import com.codemaven.liveries.db.service.TeamsService;
 import com.codemaven.liveries.manager.enums.NavBarZone;
 import com.codemaven.liveries.util.StringUtil;
 
@@ -21,7 +27,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class SeriesServlet extends ServletBase
 {
-	private static final String JSP_PATH = "series";
+	private static final String JSP_PATH = "hub/series/";
 	private ServiceFactory serviceFactory;
 
 	@Override
@@ -37,14 +43,43 @@ public class SeriesServlet extends ServletBase
 			displayError(req, resp, "Uh-Oh, something went wrong!");
 		}
 	}
-
+	
 	private void doCmd(HttpServletRequest req, HttpServletResponse resp) throws IOException
 	{
 		String cmd = getCmd(req);
-		if (StringUtil.isNullOrEmpty(cmd))
+		if (StringUtil.isEqual(cmd, "view"))
 		{
-
+			doView(req, resp);
 		}
+		else
+		{
+			displaySeries(req, resp);
+		}
+	}
+
+	private void displaySeries(HttpServletRequest req, HttpServletResponse resp)
+	{
+		List<Series> series = serviceFactory.getInstance(ServiceType.SERIES, SeriesService.class).fetchAllSeries();
+		req.setAttribute("seriesList", series);
+		String view = getParameterString(req, "view");
+		if (StringUtil.isEqual(view, "list"))
+		{
+			displayPage(req, resp, JSP_PATH + "list.jsp");
+		}
+		else
+		{
+			displayPage(req, resp, JSP_PATH + "grid.jsp");
+		}
+	}
+
+	private void doView(HttpServletRequest req, HttpServletResponse resp)
+	{
+		int seriesId = getParameterInt(req, "id");
+		Series series = serviceFactory.getInstance(ServiceType.SERIES, SeriesService.class).fetchSeriesById(seriesId);
+		req.setAttribute("selectedSeries", series);
+		List<Teams> teams = serviceFactory.getInstance(ServiceType.TEAM, TeamsService.class).fetchSeriesTeams(seriesId);
+		req.setAttribute("teams", teams);
+		displayPage(req, resp, JSP_PATH + "view.jsp");
 	}
 
 	@Override
